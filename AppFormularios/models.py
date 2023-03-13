769e6c059c2f25b3b0c862a1a0a72510d1478bc6
Franco Kral <franco_kral@hotmail.com>
2023-03-13 06:03:20 -0300

    Creacion de los modelos Alumno, Profesor y Curso

--- AppFormularios/models.py
+++ AppFormularios/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Alumno(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    dni=models.IntegerField()
+    legajo=models.IntegerField()
+    email=models.EmailField()
+    def __str__(self):
+        return f"{self.id} - {self.nombre}  {self.apellido}"
+    
+class Profesor(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    dni=models.IntegerField()
+    legajo=models.IntegerField()
+    email=models.EmailField()
+    profesion=models.CharField(max_length=30)
+    def __str__(self):
+        return f"{self.id} - {self.nombre}  {self.apellido}"
+
+class Curso(models.Model):
+    nombre=models.CharField(max_length=30)
+    año_lectivo=models.IntegerField()
+    def __str__(self):
+        return f"{self.id} - {self.nombre} - {self.año_lectivo}"
+    
